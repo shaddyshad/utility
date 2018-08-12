33944017aff0577dade4bfd0bee8b3e23cc18539
shaddy shad <mwangishadrack5@gmail.com>
2018-08-12 19:11:01 +0000

    Start move(src, dest) file

---
+++ files/move.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+/*
+* @param src {List} - a list of files to move
+* @param dest {String} - the directory to move the files
+ */
+function move_to_folder(src, dest){
+    const destDir = path.resolve(dest);
+    src.forEach(function (file) {
+
+    })
+
+}
